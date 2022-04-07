1072cdc81ae74dbf07fc0ce5eff3f921320aa44d
Nikola <nkl_andonov@yahoo.com>
2022-04-07 13:18:37 +0300

    Added and configured Bootstrap 5

--- exam_project/exam_project/settings.py
+++ exam_project/exam_project/settings.py
@@ -101,10 +101,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',
+)
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_ROOT = BASE_DIR / 'staticfiles/mediafiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
