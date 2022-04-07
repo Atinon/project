bae68ec1f9b8661f39013369b011a98100fcc853
Nikola <nkl_andonov@yahoo.com>
2022-04-07 11:19:29 +0300

    Added mediafiles folder and also to gitignore

--- exam_project/exam_project/settings.py
+++ exam_project/exam_project/settings.py
@@ -104,6 +104,8 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
