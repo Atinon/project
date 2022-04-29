c5c79c7dee42797f52818b020febd36526d64f96
Nikola <nkl_andonov@yahoo.com>
2022-04-29 21:53:13 +0300

    Patched up greet page and added an about page

--- exam_project/exam_project/main/urls.py
+++ exam_project/exam_project/main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from exam_project.main.views import IndexView, SettingsView, SearchView
+from exam_project.main.views import IndexView, SettingsView, SearchView, GreetView, AboutView
 
 urlpatterns = (
     path('', IndexView.as_view(), name='index'),
+    path('greet/', GreetView.as_view(), name='greet'),
+    path('about/', AboutView.as_view(), name='about'),
     path('settings/', SettingsView.as_view(), name='settings'),
     path('search_results/', SearchView.as_view(), name='search'),
 )
--- exam_project/exam_project/main/views.py
+++ exam_project/exam_project/main/views.py
@@ -1,25 +1,39 @@
 from functools import reduce
 
-from django.shortcuts import render
+from django.contrib.auth import mixins as auth_mixins
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views import generic as views
 from django.db.models import Q
 
 from exam_project.accounts.models import UserProfile
+from exam_project.utils.view_mixins import RedirectIfAuthenticatedMixin
 
 PROFILE = UserProfile
 
 
-# USER = get_user_model()
+class GreetView(RedirectIfAuthenticatedMixin, views.TemplateView):
+    template_name = 'main/greet.html'
+
+
+class AboutView(views.TemplateView):
+    template_name = 'main/about.html'
+
 
 class IndexView(views.TemplateView):
     template_name = 'main/index.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().dispatch(request, *args, **kwargs)
+        return redirect(reverse_lazy('greet'))
 
-class SettingsView(views.TemplateView):
+
+class SettingsView(auth_mixins.LoginRequiredMixin, views.TemplateView):
     template_name = 'main/settings.html'
 
 
-class SearchView(views.ListView):
+class SearchView(auth_mixins.LoginRequiredMixin, views.ListView):
     model = PROFILE
     template_name = 'main/search.html'
 
