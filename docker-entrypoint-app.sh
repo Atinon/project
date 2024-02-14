fb53d6c982c510772248f259f7821ba4b60f9d4d
Nikola <nkl_andonov@yahoo.com>
2024-02-15 00:29:01 +0200

    Changed entrypoint app to absolute path

--- docker-entrypoint-app.sh
+++ docker-entrypoint-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Apply database migrations
-python ./exam_project/manage.py migrate
+python /project/exam_project/manage.py migrate
 
 # Start Django server
-exec python ./exam_project/manage.py runserver 0.0.0.0:8000
+exec python /project/exam_project/manage.py runserver 0.0.0.0:8000
