dc4de43de85f99137cf5984e044240506188d7ff
Nikola <nkl_andonov@yahoo.com>
2024-02-15 15:00:33 +0200

    Added restart param to app and modified entrypoint for app to retry migrations

--- docker-entrypoint-app.sh
+++ docker-entrypoint-app.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Apply database migrations
-python /project/exam_project/manage.py migrate
+# Attempt till max_attempts to do migrations
+attempts=0
+max_attempts=4
+retry_delay=8
+
+while [ $attempts -lt $max_attempts ]; do
+    attempts=$((attempts + 1))
+    echo "Attempting to apply migrations (Attempt $attempts of $max_attempts)..."
+    python /project/exam_project/manage.py migrate --noinput && break
+    echo "Migration attempt failed. Retrying in $retry_delay seconds..."
+    sleep $retry_delay
+done
 
 # Start Django server
 exec python /project/exam_project/manage.py runserver 0.0.0.0:8000
